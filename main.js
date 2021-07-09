279a6ce9cbe82e11711fddf375130e431299852f
Omarabdelal <you@example.com>
2021-07-09 17:58:37 +0300

    d5

--- main.js
+++ main.js
@@ -1,2 +1,52 @@
 console.log(arrowTitle)
 /* START CODE UNDER THIS LINE */
+ //P.C
+ //Q1
+  /*
+ 
+ Create a global variable myFavoriteFood and return its value in the function's local scope.
+
+// make sure that the variable is in the global scope
+const favoriteFood = function () {
+  // TODO: Your code here
+};
+
+favoriteFood(); // => the value of `myFavoriteFood` variable
+ 
+
+ */ 
+let  myFavoriteFood = " big mac "
+
+const favoriteFood = function (){
+
+
+return (myFavoriteFood);
+
+}
+//Q2 
+ /*
+ 
+ Create a function updateFavoriteFood that updates the value of myFavoriteFood
+
+const updateFavoriteFood = function (newValue) {
+  // TODO: Your code here
+};
+
+updateFavoriteFood("Pizza");
+favoriteFood(); // => "Pizza"
+ 
+ 
+ */
+
+console.clear()
+
+
+let FavoriteFood = " big tasty"
+console.log (FavoriteFood);
+const updateFavoriteFood=function(){
+
+    let FavoriteFood = "pizza"
+
+    return (FavoriteFood);
+}
+
